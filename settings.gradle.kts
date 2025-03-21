b89f34915e8c25ffd5806296fc76b909b725a6ca
Jikoo <jikoo.games@gmail.com>
2025-03-21 17:27:54 -0400

    Hack around JitPack building Spigot
    and publishing it, but we don't talk about that

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,31 +3,33 @@ rootProject.name = "openinvparent"
 include(":openinvapi")
 project(":openinvapi").projectDir = file("api")
 
-val addons = listOf(
-  "togglepersist"
-)
-for (addon in addons) {
-  include(":addon$addon")
-  val proj = project(":addon$addon")
-  proj.projectDir = file("addon/$addon")
-  proj.name = "openinv$addon"
+if (!java.lang.Boolean.getBoolean("jitpack")) {
+  val addons = listOf(
+    "togglepersist"
+  )
+  for (addon in addons) {
+    include(":addon$addon")
+    val proj = project(":addon$addon")
+    proj.projectDir = file("addon/$addon")
+    proj.name = "openinv$addon"
+  }
+
+  include(":openinvcommon")
+  project(":openinvcommon").projectDir = file("common")
+
+  val internals = listOf(
+    "common",
+    "paper1_21_3",
+    "paper1_21_1",
+    "spigot"
+  )
+  for (internal in internals) {
+    include(":openinvadapter$internal")
+    project(":openinvadapter$internal").projectDir = file("internal/$internal")
+  }
+
+  include(":resource-pack")
+
+  include(":plugin")
+  project(":plugin").name = "openinvplugin"
 }
-
-include(":openinvcommon")
-project(":openinvcommon").projectDir = file("common")
-
-val internals = listOf(
-  "common",
-  "paper1_21_3",
-  "paper1_21_1",
-  "spigot"
-)
-for (internal in internals) {
-  include(":openinvadapter$internal")
-  project(":openinvadapter$internal").projectDir = file("internal/$internal")
-}
-
-include(":resource-pack")
-
-include(":plugin")
-project(":plugin").name = "openinvplugin"
